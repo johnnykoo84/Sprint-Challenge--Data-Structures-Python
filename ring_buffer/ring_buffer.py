e80bcc9ea864fa30ae083f30d93f48cc05af151f
johnnykoo84 <johnnykoo84@gmail.com>
2020-09-28 11:18:47 +0900

    Complete ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,24 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.buffer = [None] * capacity
+        self.head = 0
+        self.tail = 0
+        self.capacity = capacity
+
+    def is_full(self):
+        return self.tail > (self.head-1) % self.capacity
 
     def append(self, item):
-        pass
+
+        if self.is_full():
+            self.buffer[self.head] = item
+            self.head = (self.head + 1) % self.capacity
+        else:
+            self.buffer[self.tail] = item
+            self.tail = (self.tail + 1) % self.capacity
 
     def get(self):
-        pass
+        Not_none_values = filter(None.__ne__, self.buffer)
+
+        list_of_values = list(Not_none_values)
+        return list_of_values
